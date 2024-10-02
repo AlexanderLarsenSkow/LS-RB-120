ad1de5229707758df2197f9c4547ef6d20f4164b
Alex Skow <askow2015@gmail.com>
2024-10-02 21:54:47 +0000

    Solved Book Part 2

---
+++ OO_exercises/easy1/4.rb
@@ -0,0 +1,20 @@
+# Fix the Program: Books Part 2
+
+# Complete the Program so it accomplishes the expected output.
+
+class Book
+  attr_accessor :title, :author
+
+  def to_s
+    %("#{title}", by #{author})
+  end
+end
+
+book = Book.new
+book.author = "Neil Stephenson"
+book.title = "Snow Crash"
+puts %(The author of "#{book.title}" is #{book.author}.)
+puts %(book = #{book}.)
+
+# The author of "Snow Crash" is Neil Stephenson.
+# book = "Snow Crash", by Neil Stephenson.
